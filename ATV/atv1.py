d7b7530a9a68a3632a1bc68b23cd116f81309a6c
Felipe Batista Fraga de Sousa <145801660+felipebfs@users.noreply.github.com>
2023-09-24 21:19:08 -0300

    Add files via upload

---
+++ ATV/atv1.py
@@ -0,0 +1,8 @@
+#atv1
+
+alfabeto = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
+
+for k, v in enumerate(alfabeto):
+    print(k, v)
+
+print(alfabeto.replace(" ", "-"))
---
+++ python.py
@@ -0,0 +1,70 @@
+#nome = "Felipe Batista Fraga de Sousa"
+#nome2 = '''
+#esta é uma string longa contendo infos sobre py
+#'''
+
+#print(nome)
+############################################################
+
+#Propriedades de strings
+
+#l = 'lista de letras'
+#data = '14/09/2001'
+
+#tamanho = len(l)
+#print(tamanho)
+
+#split
+#lista = l.split(" ")
+#print(lista)
+#lista2 = data.split("/")
+#print(lista2)
+
+#substi
+#print(l.replace(" de ", ""))
+##############################################################
+
+# 012345
+#nome ='python'
+#nova_string = nome[1:2:2]
+#print(nova_string)
+
+##############################################################
+
+#comparando string
+
+#print("a"=="b")
+#print("b"!="a")
+#print("a" > "X")
+#print("a" > "1")
+
+#for i in range(122):
+#    print(str(i) + " - "+chr(i))
+
+##############################################################
+
+#nome = "python"
+#for
+#for i in nome:
+#    print(i)
+
+#print("--------")
+#while
+#i = 0
+#while i < len(nome):
+#    print(nome[i])
+#    i+=1
+#print("--------")
+#for/enumerate
+#for k, v in enumerate(nome):
+#    print(k, v)
+
+#############################################################
+
+#estrutura de listas
+
+nome = "python"
+lista = [2,3,4,5]
+lista2 = [1.5 , 2.3 , 6.5]
+lista3 = [1, 2.5, "numero"]
+lista = []
